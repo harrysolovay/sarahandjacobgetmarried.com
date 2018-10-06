35d67f3c1e70919ce539090ec1489c658958dc56
Harry Solovay <harrysolovay@gmail.com>
2018-10-06 14:13:10 -0400

    updated site

--- src/pages/Activities/index.js
+++ src/pages/Activities/index.js
@@ -1,6 +1,14 @@
-import { Layout } from '~/components'
+import { Layout, ExternalLink } from '~/components'
+
+const style = { display: 'block', color: '#000', marginTop: '20px' }
 
 export default () =>
   <Layout>
-    <div>Activities</div>
+    <h1>Activities</h1>
+    <div>
+      <ExternalLink { ...{ style }} href='https://www.barnesfoundation.org/'>• Barnes Museum / classes / events</ExternalLink>
+      <ExternalLink { ...{ style }} href='https://www.nmajh.org/'>• National Museum of American Jewish History</ExternalLink>
+      <ExternalLink { ...{ style }} href='https://constitutioncenter.org/'>• National Constitution Center</ExternalLink>
+      <ExternalLink { ...{ style }} href='https://www.visitphilly.com/'>• More activities on visitphiladelphia.com</ExternalLink>
+    </div>
   </Layout>
